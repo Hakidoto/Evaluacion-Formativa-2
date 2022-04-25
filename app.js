3037ebab50b58586daff39a882190e038042d033
Hadid Garcia <hadid.garcia1@gmail.com>
2022-04-25 01:23:50 -0500

    Actualizacion
    -Corregido NaN al sacar figuras.
    - Diseño mejorado, supongo :>
    
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-let TotalPtsJugador = 0
-let TotalPtsComputadora = 0
-let baraja = [];
+let TotalPtsJugador = 0;
+let TotalPtsComputadora = 0;
+let cartas = [];
 let carta;
-const naipes =['C', 'D', 'H', 'S'];
+const naipes = ['K', 'Q', 'J', 'A'];
 
+CrearBaraja();
 
-$('#btn-nuevo').click(function(){
- IniciarJuego();
+$('#btn-nuevo').click(function () {
+    IniciarJuego();
 });
 
-$('#btn-carta').click(function(){
+$('#btn-carta').click(function () {
     turnoJugador();
 });
 
-$('#btn-detener').click(function(){
+$('#btn-detener').click(function () {
     $('#btn-carta').attr('disabled', 'true')
     $('#btn-detener').attr('disabled', 'true')
     turnoComputadora();
@@ -21,85 +22,7 @@ $('#btn-detener').click(function(){
 });
 
 
-
-function CrearBaraja(){
-    baraja = [];
-    const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const figuras = ['J', 'Q', 'K', 'A'];
-    const palos =['C', 'D', 'H', 'S'];
-
-    for (const n of numeros) {
-        for(const p of palos){
-            baraja.push(n + p);
-        }
-    }
-    figuras.forEach((l)=> {
-        palos.forEach((p) => {
-            baraja.push(l+p);
-        })
-    })
-
-    baraja = _.shuffle(baraja)
-    console.log(baraja);
-}
-
-function valor(carta){
-    let valorCarta = carta.substring(0, carta.length - 1);
-    
-    if(naipes.includes(valorCarta) && valorCarta ==='A'){
-        return(valorCarta === 'A'  && (TotalPtsComputadora>21 || TotalPtsJugador>21)) ? 1 : 11;
-    } 
-    else if(naipes.includes(valorCarta) && valorCarta !='A'){
-        return valorCarta = 10
-    }
-    else {
-        return parseInt (valorCarta);
-    }
-
-}
-function turnoJugador(){
-    carta = baraja.shift();
-    $('#cartasJugador').html($('#cartasJugador').html() + `<img src='./imagenes/cartas/${carta}.png'>`); 
-
-    TotalPtsJugador += valor(carta);
-    $('#puntosJugador').text(TotalPtsJugador);
-
-    if( TotalPtsJugador > 21){
-        $('#btn-card').attr('disabled', 'true');
-        $('#btn-stop').attr('disabled', 'true');
-        turnoComputadora();
-    }
-}
-function turnoComputadora(){
-    let ganaJugador = true;
-    do {
-        carta = baraja.shift();
-        $('#cartasComputadora').html($('#cartasComputadora').html() + `<img src='./imagenes/cartas/${carta}.png'>`); 
-
-        TotalPtsComputadora += valor(carta);
-        $('#puntosComputadora').text(TotalPtsComputadora);
-        if(TotalPtsJugador > 21){
-            ganaJugador = false;
-            mensajeJugador(ganaJugador ? 'El jugador gano' : 'La computadora gana');
-            break;
-        }
-    } while (TotalPtsComputadora <= 21 && TotalPtsComputadora < TotalPtsJugador) ;
-
-    if(TotalPtsComputadora <= 21 && TotalPtsComputadora >= TotalPtsComputadora){
-        ganaJugador = false;
-        mensajeJugador(ganaJugador ? 'El jugador gano' : 'La computadora gana');
-        
-    }
-    mensajeJugador(ganaJugador ? 'El jugador gano' : 'La computadora gana');
-}
-
-function mensajeJugador(mensajeJugador){
-    $('#mensajeGanador').text(mensajeJugador)
-    $('#mensajeGanador').removeClass('hidden');
-    
-}
-
-function IniciarJuego(){
+function IniciarJuego() {
     $('#cartasJugador').html('');
     $('#cartasComputadora').html('');
     console.clear();
@@ -108,6 +31,84 @@ function IniciarJuego(){
     TotalPtsComputadora = 0;
     $('#puntosJugador').text(TotalPtsJugador);
     $('#puntosComputadora').text(TotalPtsComputadora);
+    $('#btn-carta').removeAttr('disabled');
+    $('#btn-detener').removeAttr('disabled');
+    $('#mensajeGanador').addClass('hidden');
+
+}
+
+function CrearBaraja() {
+    cartas = [];
+    const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const figuras = ['J', 'Q', 'K', 'A'];
+    const palos = ['C', 'D', 'H', 'S'];
+
+    for (const n of numeros) {
+        for (const p of palos) {
+            cartas.push(n + p);
+        }
+    }
+    figuras.forEach((l) => {
+        palos.forEach((p) => {
+            cartas.push(l + p);
+        })
+    })
+
+    cartas = _.shuffle(cartas)
+    console.log(cartas);
+}
+
+function valor(carta) {
+    let valorCarta = carta.substring(0, carta.length - 1);
+
+    if (naipes.includes(valorCarta) && valorCarta === 'A') {
+        return (valorCarta === 'A' && (TotalPtsComputadora > 21 || TotalPtsJugador > 21)) ? 1 : 11;
+    }
+    else if (naipes.includes(valorCarta) && valorCarta != 'A') {
+        return valorCarta = 10
+    }
+    else {
+        return parseInt(valorCarta);
     }
 
-CrearBaraja();
+}
+function turnoJugador() {
+    carta = cartas.shift();
+    $('#cartasJugador').html($('#cartasJugador').html() + `<img src='./imagenes/cartas/${carta}.png'>`);
+
+    TotalPtsJugador += valor(carta);
+    $('#puntosJugador').text(TotalPtsJugador);
+
+    if (TotalPtsJugador > 21) {
+        $('#btn-carta').attr('disabled', 'true');
+        $('#btn-detener').attr('disabled', 'true');
+        turnoComputadora();
+    }
+}
+function turnoComputadora() {
+    let ganaJugador = true;
+    do {
+        carta = cartas.shift();
+        $('#cartasComputadora').html($('#cartasComputadora').html() + `<img src='./imagenes/cartas/${carta}.png'>`);
+
+        TotalPtsComputadora += valor(carta);
+        $('#puntosComputadora').text(TotalPtsComputadora);
+        if (TotalPtsJugador > 21) {
+            ganaJugador = false;
+            mensajeJugador(ganaJugador ? 'El jugador gana' : 'La computadora gana');
+            break;
+        }
+    } while (TotalPtsComputadora <= 21 && TotalPtsComputadora < TotalPtsJugador);
+
+    if (TotalPtsComputadora <= 21 && TotalPtsComputadora >= TotalPtsComputadora) {
+        ganaJugador = false;
+        mensajeJugador(ganaJugador ? 'El jugador gana' : 'La computadora gana');
+    }
+    mensajeJugador(ganaJugador ? 'El jugador gana' : 'La computadora gana');
+
+    function mensajeJugador(mensajeJugador) {
+        $('#mensajeGanador').text(mensajeJugador)
+        $('#mensajeGanador').removeClass('hidden');
+
+    }
+}
